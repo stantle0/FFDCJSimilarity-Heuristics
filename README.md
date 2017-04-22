e9324c003e5036cb35280379b30ad9c034f89fd1
Gabriel Medeiros <gigabytegabriel@gmail.com>
2017-04-22 19:00:33 -0400

    creating test001

--- README.md
+++
@@ -1,1 +0,0 @@
-# FFDCJSimilarity-Heuristics
---
+++ test001_original_graph.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <iostream>
+#include "graph.hpp"
+#include "paths-cycles.hpp"
+
+using namespace std;
+
+int main ()
+
+{
+    char nome[10];
+    //Graph *graph = new Graph();
+    Graph *graph = new Graph("grafo de testes", 10000);
+    cout << "teste" << endl;
+
+    //alocar vértices
+
+
+
+    //alocar extremidades
+
+
+
+    //associar extremidades aos vértices
+
+
+
+    //alocar arestas
+
+
+    //associar arestas aos vértices
+
+
+
+    //associar arestas às extremidades
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
